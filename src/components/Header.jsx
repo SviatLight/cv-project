a85f5ff26d3dbd0161264d90c8e7b0b0c6e8a900
SviatLight <alex0fasten@gmail.com>
2022-10-01 15:50:40 +0300

    improved headers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({ title, description }) => {
   return (
     <div>
       <p className="text-4xl font-bold inline border-b-4 border-pink-600">{title}</p>
-      <p className="py-4">// {description}</p>
+      <p className="py-4">{`// ${description}`}</p>
     </div>
   );
 };
